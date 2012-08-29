5ffb8ebe22291af083dfa676da314e956b8642c1
Max Woolf <maximus@zingysaturn.co.uk>
2012-08-29 11:29:25 +0100

    latest version

--- wadt.php
+++ wadt.php
@@ -57,6 +57,8 @@ function build_menu_page()
 			echo "DELETE FROM ". $wpdb->prefix . "wadt WHERE id=". $_POST['delete'];
 	    }
     }
+
+		$actions = $wpdb->get_results("SELECT * FROM ". $wpdb->prefix . "wadt");
 	//Get any current stats in the database    
 	echo "<div class='wrap'><div class='icon32' style='background-image:url(../wp-content/plugins/podnoshstats/pn-logo-wp-big.png)'></div><h2>We're already doing this!</h2>";
     echo "<table class='widefat'>
@@ -108,13 +110,13 @@ function create_wadt_post_form()
 	global $post;
 	echo $content;
 	echo "<br />";
-	echo "<h2 id='wadt-form'>Do you or your organisation already do this?</h2><p>Let everyone know what is already happening in the city. (Any information you add will be shown on this page and on our <a href='http://fairbrum.podnosh.com/were-already-doing-it/'>map</a>.)</p>";
+	echo "<h2 id='wadt-form'>Do you or your organisation already do this?</h2><p>Let everyone know what is already happening in the city. (Any information you add will be shown on this page and on our <a href='http://fairbrum.podnosh.com/were-already-doing-it/'>map</a>.) All of these fields are <i>optional</i> except for the description.</p>";
 	echo "<form method='POST'><input type='text' name='title' placeholder='Name'>";
 	echo "<input type='text' name='org_name' placeholder='Your Organisation'><br />";
 	echo "<input type='text' name='website' placeholder='Website'>";
 	echo "<input type='text' name='postcode' placeholder='Postcode'><br />";
 	echo "<input type='text' name='telephone' placeholder='Phone Number'><br />";
-	echo "<textarea rows='10' cols='10' name='description' placeholder='Describe how you or your organisation are already doing this recommendation.'></textarea>";
+	echo "<textarea rows='5' cols='45' name='description' placeholder='Describe how you or your organisation are already doing this recommendation.'></textarea><br />";
 	echo "<input type='hidden' name='post_id' value='".$post->ID."'>";
 	echo "<input type='submit' value='Save'>";
 	echo "</form>";
@@ -171,6 +173,21 @@ function wadt_map() {
 		}else{
 			$url = $data->website;
 		}
+		if (substr($data->website, 0, 7) != "http://") {
+			$url = "http://" . $data->website;
+		}else{
+			$url = $data->website;
+		}
+		if($data->org_name != "")
+		{
+		  $name =  $data->org_name ." is already doing the recommendation ";
+		}else if($data->title != "")
+		{
+		  $name =  $data->title ." is already doing the recommendation ";
+		}else{
+		  $name =  "Anonymous is already doing the recommendation ";
+		}
+		$current_post = get_post($data->post_id);
     	echo "
         geocoder.geocode( { 'address': '". $data->postcode ."'}, function(results, status) {
           if (status == google.maps.GeocoderStatus.OK) {
@@ -182,7 +199,7 @@ function wadt_map() {
                 position: results[0].geometry.location
             });
             var infowindow = new google.maps.InfoWindow({
-            content: '<a href=\"".$url."\" target=\"_blank\">".$data->org_name."</a> already <a href=\"".get_permalink($data->post_id)."\">".get_the_title($data->post_id)."</a>'
+            content: '".$name." ".$current_post->post_title.". <br /> ".addslashes($data->description)." <a href=\'".get_permalink($data->post_id)."\'>Read more...</a><br />Tel: ".$data->telephone ." - Website: <a href=\'".$url."\' target=\'_blank\'>".$url."</a>'
         });
         
         google.maps.event.addListener(marker, 'click', function() {
@@ -220,8 +237,20 @@ function show_wadt()
 		}else{
 			$url = $result->website;
 		}
-		echo "<a href='".$url."'>".$result->org_name ."</a> is already doing this.<br />";
+		if($result->org_name != "")
+		{
+		  echo "<a href='".$url."'>".$result->org_name ."</a> is already doing this.<br />";
+		}else if($result->title != "")
+		{
+		  echo "<a href='".$url."'>".$result->title ."</a> is already doing this.<br />";
+		}else{
+		  echo "Anonymous is already doing this.<br />";
+		}
+		
+		
 		echo $result->description;
+		echo "<hr />";
+		echo "<p class='comment-date'>Tel: " . $result->telephone. " -  Website: <a href='".$url."' target='_blank'>" . $url ."</a> - Postcode: <a href='http://fairbrum.podnosh.com/were-already-doing-it/'>".$result->postcode."</a></p>";
 		echo "</div>";
 	}
 	echo "</li>";
@@ -231,10 +260,13 @@ function show_wadt()
 function wadt_link($content)
 {
   define( 'WADT_PATH', plugin_dir_url(__FILE__) );
-	
-	$content = $content .= "<br /><a href='#respond'><img src='".WADT_PATH."commentbubble.png' style='float:left;'></a>";
-	$content .= "<a href='#wadt-form'><img src='".WADT_PATH."organisationbubble.png' style='float:right;'></a>";
-	return $content;
+	if(get_post_type(get_the_ID())== "post"){
+		$content = $content .= "<br /><a href='#respond'><img src='".WADT_PATH."commentbubble.png' style='float:left;'></a>";
+		$content .= "<a href='#wadt-form'><img src='".WADT_PATH."organisationbubble.png' style='float:right;'></a>";
+		return $content;
+	}else{
+		return $content;
+	}
 }
 
 function wadt_full_table()
@@ -258,8 +290,24 @@ function wadt_full_table()
 		}else{
 			$url = $result->website;
 		}
-		echo "<a href='".$url."'>".$result->org_name ."</a> is already doing this.<br />";
+		if (substr($result->website, 0, 7) != "http://") {
+			$url = "http://" . $result->website;
+		}else{
+			$url = $result->website;
+		}
+		if($result->org_name != "")
+		{
+		  $name =  $result->org_name ." is already doing the recommendation ";
+		}else if($result->title != "")
+		{
+		  $name =  $result->title ." is already doing the recommendation ";
+		}else{
+		  $name =  "Anonymous is already doing the recommendation ";
+		}
+		$current_post = get_post($result->post_id);
+		echo "<a href='".$url."'>".$result->org_name ."</a> is already doing the recommendation: <a href=\"".get_permalink($current_post->ID)."\" target=\"_blank\">".$current_post->post_title."</a><br />";
 		echo $result->description;
+		echo "<br />Tel: " . $result->telephone. " -  Website: <a href='".$url."' target='_blank'>" . $url ."</a> - Postcode: <a href='http://fairbrum.podnosh.com/were-already-doing-it/'>".$result->postcode."</a>";
 		echo "</div>";
 	}
 	echo "</li>";
@@ -325,7 +373,7 @@ function wadt_recent_tweets()
    }
 }
 
-function reccomendations_page()
+function recommendations_page()
 {
 	global $wpdb;
 	$args = array('post_type' => 'post');
@@ -334,10 +382,10 @@ function reccomendations_page()
 	{
 		//echo var_dump($post) . "<br />";
 		echo "<h2><a href=\"".get_permalink($post->ID)."\">  ". $post->post_title . "</a></h2>";
-		echo "<p><a href=\"".get_permalink($post->ID)."\">Read the recommendation...</a></p>";
+		echo "<p><a href=\"".get_permalink($post->ID)."\">Read the recommendation &raquo;</a></p>";
 		echo "<div class='post-meta'>";
 		$categories = get_the_category($post->ID);
-		echo "<p>Line of enquiry: ";
+		echo "<p>Posted in ";
 		foreach($categories as $category)
 		{
 			echo "<a href=\"".get_category_link($category->cat_ID)."\">".$category->cat_name."</a> ";
@@ -376,7 +424,7 @@ add_shortcode('wadt_newest_comments', 'wadt_newest_comments');
 add_shortcode('wadt_popular_posts_list', 'wadt_popular_posts_list');
 add_shortcode( 'wadt_map', 'wadt_map' );
 add_shortcode('wadt_full_table', 'wadt_full_table');
-add_shortcode('reccomendations_page', 'reccomendations_page');
+add_shortcode('recommendations_page', 'recommendations_page');
 
 //Installation hooks
 register_activation_hook(__FILE__,'wadt_install');
@@ -386,9 +434,9 @@ register_activation_hook(__FILE__, 'build_menu');
 add_action( 'admin_menu', 'build_menu' );
 
 //Public facing hooks
-add_action('comment_form_before', 'create_wadt_post_form');
+add_action('comment_form_after', 'create_wadt_post_form');
 add_action('wp_loaded', 'submit_wadt');
 add_action('wp_head', 'add_gmaps_js');
-add_action('comment_form_before', 'show_wadt');
+add_action('comment_form_after', 'show_wadt');
 add_filter('the_content', 'wadt_link');
 ?>
