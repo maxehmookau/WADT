e4bd7fd94552ad31cf6615773ef381ab99936c38
Max Woolf <maximus@zingysaturn.co.uk>
2012-08-29 11:59:24 +0100

    added google analytics tracking to head tag

--- wadt.php
+++ wadt.php
@@ -141,6 +141,19 @@ function submit_wadt()
 function add_gmaps_js()
 {
 	echo "<script type='text/javascript' src='http://maps.googleapis.com/maps/api/js?key=AIzaSyAJU5MBDWPhAnjDe7sOMIhCYR4mH2fvzk8&sensor=false'></script>";
+	echo "<script type='text/javascript'>
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-34413982-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>";
 	//echo "<style type='text/css'>html { height: 100% }body { height: 100%; margin: 0; padding: 0 }#map_canvas { height: 100% }</style>";
 	
 }
